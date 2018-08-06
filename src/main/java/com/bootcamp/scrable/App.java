7d3f8da9f4d071aaa05e909b762f8aa5e45567ee
Kumar <skumar15@lm-maa-22004998.paypalcorp.com>
2018-08-06 14:21:27 +0530

    inital commit

---
+++ src/main/java/com/bootcamp/scrable/App.java
@@ -0,0 +1,13 @@
+package com.bootcamp.scrable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
